679734fac05de15240a6eaa4b1edb88cf862dd7c
lars-gentsch-thb <gentsch@thb.de>
2020-10-21 22:11:55 +0200

    updated to java 14 and most recent version of junit.

--- src/test/java/tdd/steps/PalindromeCheckerStep.java
+++ src/test/java/tdd/steps/PalindromeCheckerStep.java
@@ -1,6 +1,6 @@
 package tdd.steps;
 
-import io.cucumber.java.en.Given;;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.When;
 import io.cucumber.java.en.Then;
 import tdd.PalindromChecker;
